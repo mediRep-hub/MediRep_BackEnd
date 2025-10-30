5ed5ec0f20ed9816f98b9959886ac905c3be6f1b
gccberryb-alt <gccberryb@gmail.com>
2025-10-30 17:34:41 +0500

    update

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://medi-rep-front-end.vercel.app/",
+  "https://medi-rep-front-end.vercel.app",
 ];
 
 app.use(
