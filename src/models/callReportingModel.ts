37e36ec809c794fbfc34088389b0fac6c9f28607
gccberryb-alt <gccberryb@gmail.com>
2025-11-10 11:58:42 +0500

    updat e

--- src/models/callReportingModel.ts
+++ src/models/callReportingModel.ts
@@ -7,78 +7,24 @@ const callReportSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
-    mrName: {
-      type: String,
-      required: true,
-    },
-    doctorName: {
-      type: String,
-      required: true,
-    },
-    doctorAddress: {
-      type: String,
-      required: true,
-    },
-    strategyName: {
-      type: String,
-      required: true,
-    },
-    date: {
-      type: Date,
-      required: true,
-    },
-    checkIn: {
-      type: String,
-      required: true,
-    },
-    checkOut: {
-      type: String,
-      required: true,
-    },
-    duration: {
-      type: String,
-      required: true,
-    },
-    productDiscussed: {
-      type: String,
-      required: true,
-    },
-    doctorResponse: {
-      type: String,
-      required: true,
-    },
-    promotionalMaterialGiven: {
-      type: String,
-      required: true,
-    },
-    followUpRequired: {
-      type: String,
-      required: true,
-    },
-    doctorPurchaseInterest: {
-      type: String,
-      required: true,
-    },
-    nextVisitDate: {
-      type: Date,
-      required: true,
-    },
-    keyDiscussionPoints: {
-      type: String,
-      required: true,
-    },
-    doctorConcerns: {
-      type: String,
-      required: true,
-    },
-    area: {
-      type: String,
-      required: true,
-    },
-    discustionType: {
-      type: String,
-      required: true,
-    },
+    mrName: { type: String, required: true },
+    doctorName: { type: String, required: true },
+    doctorAddress: { type: String, required: true },
+    strategyName: { type: String, required: true },
+    date: { type: Date, required: true },
+    checkIn: { type: String, required: true },
+    checkOut: { type: String, required: true },
+    duration: { type: String, required: true },
+    productDiscussed: { type: String, required: true },
+    doctorResponse: { type: String, required: true },
+    promotionalMaterialGiven: { type: String, required: true },
+    followUpRequired: { type: Boolean, default: false },
+    doctorPurchaseInterest: { type: String, required: true },
+    nextVisitDate: { type: Date },
+    keyDiscussionPoints: { type: String, required: true },
+    doctorConcerns: { type: String, required: true },
+    area: { type: String, required: true },
+    discussionType: { type: String, required: true },
     checkInLocation: {
       lat: { type: Number, required: true },
       lng: { type: Number, required: true },
