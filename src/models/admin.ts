cdbdbeecef3d1b8119b22b0da9ffef9219a24f22
gccberryb-alt <gccberryb@gmail.com>
2026-01-14 12:36:48 +0500

    update

--- src/models/admin.ts
+++ src/models/admin.ts
@@ -1,5 +1,7 @@
 import mongoose, { Document, Schema, Model, Types } from "mongoose";
 
+// ------------------ Interfaces ------------------
+
 export interface IAdmin extends Document {
   _id: Types.ObjectId;
   adminId: string;
@@ -13,11 +15,13 @@ export interface IAdmin extends Document {
   region: string;
   strategy: string;
   position: string;
+  ownerName: string;
+  fcmToken?: string;
   createdAt?: Date;
   updatedAt?: Date;
-  ownerName: string;
 }
 
+// ------------------ Main Admin Schema ------------------
 const adminSchema: Schema<IAdmin> = new Schema(
   {
     adminId: { type: String, required: true, unique: true },
@@ -26,13 +30,12 @@ const adminSchema: Schema<IAdmin> = new Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     image: { type: String },
-
+    fcmToken: { type: String },
     division: { type: String, required: true },
     area: { type: String, required: true },
     region: { type: String, required: true },
     strategy: { type: String, required: true },
     position: { type: String, required: true },
-
     ownerName: {
       type: String,
       required: function () {
@@ -40,11 +43,10 @@ const adminSchema: Schema<IAdmin> = new Schema(
       },
     },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
+// ------------------ Model ------------------
 const Admin: Model<IAdmin> = mongoose.model<IAdmin>("Admin", adminSchema);
 
 export default Admin;
